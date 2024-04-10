d89dc46c656af0aa488dd51d5d17489afbc13374
Luis Bustamante <luisebt995@gmail.com>
2024-04-10 18:26:21 +0200

    Initial Commit

---
+++ PropertyWrapperHW/PropertyWrapperHWApp.swift
@@ -0,0 +1,17 @@
+//
+//  PropertyWrapperHWApp.swift
+//  PropertyWrapperHW
+//
+//  Created by Luis on 10/4/24.
+//
+
+import SwiftUI
+
+@main
+struct PropertyWrapperHWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
